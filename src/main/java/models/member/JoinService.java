0f309fcd9fd1f10661e175b0c50382859b36f1f2
leejinsol234 <desirable7@naver.com>
2023-10-24 15:46:15 +0900

    JoinService 수정

--- src/main/java/models/member/JoinService.java
+++ src/main/java/models/member/JoinService.java
@@ -6,10 +6,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import java.util.Objects;
 
 public class JoinService {
-    private Validator validator;
+    private Validator<Member> validator;
     private MemberDao memberDao;
 
-    public JoinService(Validator validator,MemberDao memberDao){ //전략 패턴(확장에 열려 있는 구조)
+    public JoinService(Validator<Member> validator,MemberDao memberDao){ //전략 패턴(확장에 열려 있는 구조)
         this.validator = validator;
         this.memberDao = memberDao;
     }
